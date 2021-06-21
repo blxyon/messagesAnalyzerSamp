e1aa7a238008f2d247be080cb012ee9fa02ba04b
Valentin I Burlacu <blxyon@yahoo.com>
2021-06-21 01:54:40 +0100

    Add files via upload

--- analyzer.py
+++ analyzer.py
@@ -1,9 +1,14 @@
 import time
+import winsound
+
+
 listAnalized=[]
 linesWOTime=[]
 counterShown=0
 people=[]
-keywords=[["inf","infernus"],["cumpar","cump","buy"]]
+keywords=[[["inf","infernus"],["cumpar","buy","cump"]],
+          [["inf","infernus"],["vand","vind","sell","v/t"]],
+          [["mave","mav","maverick"],["vand","v/t","vind","sell"]]]
 def getLines():
     f = open('chatlog.txt')
     line = f.readline()
@@ -22,14 +27,20 @@ def getLines():
 def analyseForKeywords(keywords, string):
     i=0
     counter=0
-    for keys in keywords:
-        i=0
-        while(i<len(keys)):
-            if keys[i] in string:
-                i=len(keys)
-                counter=counter+1
-            i=i+1
-    if(counter==len(keywords)):
+    tempPosibilityLen=0
+    for possibility in keywords:
+        if(counter==len(possibility)):
+            return True
+        counter=0
+        for keys in possibility:
+            i=0
+            while(i<len(keys)):
+                if keys[i] in string:
+                    i=len(keys)
+                    counter=counter+1
+                i=i+1
+        tempPosibilityLen=len(possibility)
+    if(counter==len(keywords[0])):
         return True
     else:
         return False
@@ -42,9 +53,10 @@ def checkDuplicate(line):
 getLines()
 
 while(True):
-    time.sleep(3)
+    time.sleep(30)
     getLines()
     if(counterShown<len(listAnalized)):
         for i in range(counterShown,len(listAnalized)):
             print(listAnalized[i])
+            winsound.PlaySound("sound2.wav",winsound.SND_ASYNC)
         counterShown=len(listAnalized)
